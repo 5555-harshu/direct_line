d77e133551965011cf552552b366e4460acf47e4
5555-harshu <69507953+5555-harshu@users.noreply.github.com>
2020-08-11 12:40:29 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,3 +1,9 @@
+# direct_line test code Assumption
+
+# I assume screen temprature is the temprature on that date. However data is not looking correct as there are many things.
+# I did not calculate temprature using the attributes given in the input file, if that was required.
+# There must be some formula to calculte the temprature for that date as SCREEN TEMPRATURE IS DIFFERENT for different Forecast sites for the same date as Obvercation time is also not present in data.
+# -99 temprature is not possible as baltasound, does not make sense to me.
 import os
 import sys
 import pandas as pd
